aab0df130601720c41193df87cd9a2f16edc78b4
Michael Vogt <mvo@debian.org>
2013-07-15 19:08:48 +0200

    make admin interface nicer

--- chpasswd/admin.py
+++ chpasswd/admin.py
@@ -4,6 +4,17 @@ from models import (
     PasswordChangeLog,
 )
 
-admin.site.register(ADUser)
-admin.site.register(PasswordChangeLog)
+class ADUserAdmin(admin.ModelAdmin):
+    search_fields = ('username',)
+    list_display = ('username',)
+
+class PasswordChangeLogAdmin(admin.ModelAdmin):
+    search_fields = ('ad_user__username', 'source_ip')
+    list_display = ('ad_user', 'source_ip', 'when', 'success')
+    list_filter = ('success', )
+    ordering = ('when', )
+
+
+admin.site.register(ADUser, ADUserAdmin)
+admin.site.register(PasswordChangeLog, PasswordChangeLogAdmin)
 
