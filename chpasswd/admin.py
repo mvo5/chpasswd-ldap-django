f015bfff61c2316b99ba127cae5a04de44167ac4
Michael Vogt <mvo@debian.org>
2013-07-15 22:43:23 +0200

    make all model fields readonly (its a log afterall)

--- chpasswd/admin.py
+++ chpasswd/admin.py
@@ -7,12 +7,15 @@ from models import (
 class ADUserAdmin(admin.ModelAdmin):
     search_fields = ('username',)
     list_display = ('username',)
+    readonly_fields = ('username',)
+
 
 class PasswordChangeLogAdmin(admin.ModelAdmin):
     search_fields = ('ad_user__username', 'source_ip')
     list_display = ('ad_user', 'source_ip', 'when', 'success')
     list_filter = ('success', )
     ordering = ('when', )
+    readonly_fields = ('ad_user', 'source_ip', 'when', 'success', 'fail_reason')
 
 
 admin.site.register(ADUser, ADUserAdmin)
