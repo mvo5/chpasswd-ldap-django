d541b56f579a976a10a6150b9220517a0862445c
Michael Vogt <mvo@debian.org>
2013-10-24 10:38:06 +0200

    do not use sqlite3 in production

--- django_project/settings_production.py
+++ django_project/settings_production.py
@@ -2,3 +2,16 @@ from settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chpasswd_db',
+        'USER': 'chpasswd',
+        'PASSWORD': open(os.path.join(
+            os.path.dirname(__file__), "db-password")).read(),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
